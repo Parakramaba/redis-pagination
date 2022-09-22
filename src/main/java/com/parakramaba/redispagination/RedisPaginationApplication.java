d151ceaa685b53466f3e1c3dad72068d5dc9735a
Parakramaba <p.vitharana.pv@gmail.com>
2022-09-22 13:14:45 +0530

    Initial commit

---
+++ src/main/java/com/parakramaba/redispagination/RedisPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.parakramaba.redispagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisPaginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parakramaba/redispagination/RedisPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parakramaba.redispagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
